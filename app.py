cdc613bf61afbb6afeaa4a629b9902a6d3ced7c7
h.takahashi <h.takahashi.sys22@morijyobi.ac.jp>
2023-07-10 12:36:52 +0900

    新規ファイルの作成

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-import db, string, random
+from flask import Flask, render_template, request, redirect, url_for
+import db
 
 app = Flask(__name__)
-app.secret_key = ''.join(random.choices(string.ascii_letters, k=256))
 
 @app.route('/', methods=['GET'])
 def index():
@@ -10,34 +9,14 @@ def index():
     
     if msg == None:
         return render_template('index.html')
-    else:
+    else :
         return render_template('index.html', msg=msg)
 
 @app.route('/', methods=['POST'])
 def login():
-    mail = request.form.get('mail')
-    password = request.form.get('password')
-
-    if db.login(mail, password):
-        session['user'] = True # session にキー：'user', バリュー:True を追加
-        return redirect(url_for('mypage'))
-    else :
-        error = 'ログインに失敗しました。'
-        return render_template('index.html', error=error)
-
-@app.route('/mypage', methods=['GET'])
-def mypage():
-# session にキー：'user' があるか判定
-    if 'user' in session:
-        return render_template('mypage.html') # session があればmypage.html を表示
-    else :
-        return redirect(url_for('index')) # session がなければログイン画面にリダイレクト
-    
-@app.route('/logout')
-def logout():
-    session.pop('user', None) # session の破棄
-    return redirect(url_for('index')) # ログイン画面にリダイレクト
-
+    error = 'ログインに失敗しました。'
+    return render_template('index.html', error=error)
+ 
 @app.route('/register')
 def register_form():
     return render_template('register.html')
@@ -48,6 +27,13 @@ def register_exe():
     mail = request.form.get('mail')
     password = request.form.get('password')
     
+    if user_name == '':
+        error = 'ユーザ名が未入力です。'
+        return render_template('register.html', error=error, user_name=user_name, password=password)
+    if password == '':
+        error = 'パスワードが未入力です。'
+        return render_template('register.html', error=error, user_name=user_name, password=password)
+    
     count = db.insert_user(user_name, mail, password)
     
     if count == 1:
@@ -56,7 +42,6 @@ def register_exe():
     else:
         error = '登録に失敗しました。'
         return render_template('register.html', error=error)
-
+ 
 if __name__ == '__main__':
     app.run(debug=True)
-    
--- db.py
+++ db.py
@@ -5,8 +5,8 @@ def get_connection():
     connection = psycopg2.connect(url)
     return connection
 
-def insert_user(user_name, mail, password):
-    sql = 'INSERT INTO user_account VALUES (default, %s, %s, %s, %s)'
+def insert_user(user_name, password):
+    sql = 'INSERT INTO user_account VALUES (default, %s, %s, %s)'
 
     salt = get_salt() # ソルトの生成
     hashed_password = get_hash(password, salt) # 生成したソルトでハッシュ
@@ -14,7 +14,7 @@ def insert_user(user_name, mail, password):
     try : # 例外処理
         connection = get_connection()
         cursor = connection.cursor()
-        cursor.execute(sql, (user_name, mail, password, salt))
+        cursor.execute(sql, (user_name, password, salt))
         count = cursor.rowcount # 更新件数を取得
         connection.commit()
         
